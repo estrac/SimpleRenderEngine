c10c99de499f882f2e6b22ebf1ec30754ce7af8c
estrac <erikstrack@yahoo.com>
2022-12-04 18:05:29 -0700

    Updated to latest versions of all submodules (glm, googletest, imgui)

--- src/sre/Inspector.cpp
+++ src/sre/Inspector.cpp
@@ -903,7 +903,7 @@ namespace sre {
         ImGuiIO& io = ImGui::GetIO();
         if (io.KeyCtrl){
             for (int i=SDLK_1;i<SDLK_9;i++){
-               if (ImGui::IsKeyPressed(i)){
+               if (ImGui::IsKeyPressed((ImGuiKey)i)){ // TO DO: remove cast
                    selectedShader = i-SDLK_1;
                    updatedShader = true;
                }
@@ -919,7 +919,8 @@ namespace sre {
         if (ImGui::IsItemHovered())
             ImGui::SetTooltip("CTRL+S");
 
-        if (io.KeyCtrl && ImGui::IsKeyPressed(SDLK_s)){
+        if (io.KeyCtrl && ImGui::IsKeyPressed((ImGuiKey)SDLK_s)){ // TO DO:
+                                                                  // remove cast
             compile = true;
         }
         // update if compile or shader type changed or showPrecompiled is selected
